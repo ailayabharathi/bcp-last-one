f39e1d0a89dc1fd893b21406a9d54398ce075147
[dyad] <ilayabharathi8888@gmail.com>
2025-10-01 07:51:24 +0530

    [dyad] Fixed JSX syntax error in ManageFaculties - wrote 1 file(s)

--- src/pages/admin/ManageFaculties.tsx
+++ src/pages/admin/ManageFaculties.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -242,7 +242,7 @@ const ManageFaculties = () => {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required={!editingFaculty}
-                        autoComplete="new-password" {/* Added autocomplete attribute */}
+                        autoComplete="new-password"
                       />
                       <Button
                         type="button"
